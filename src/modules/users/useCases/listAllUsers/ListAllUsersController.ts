08815907d20e97e22dea6d5336dc668965b68691
Cleverson Funaki <cleverson.funaki@gmail.com>
2021-09-21 18:55:35 -0300

    Implementações do desafio 01 - Chapter II

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,20 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers;
+
+        const users = this.listAllUsersUseCase.execute({ user_id });
+
+        let returnCode = 200;
+        if (!users) {
+            returnCode = 400;
+        }
+
+        return response.status(returnCode).json(users);
+    }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,23 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+        const user = this.showUserProfileUseCase.execute({ user_id });
+        let returnCode = 200;
+        let message = {};
+
+        if (!user) {
+            returnCode = 404;
+            message = { error: "User not found!" };
+        } else {
+            message = user;
+        }
+
+        return response.status(returnCode).json(message);
+    }
 }
 
 export { ShowUserProfileController };
